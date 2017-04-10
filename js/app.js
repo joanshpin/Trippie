eb085001ae6c91b77104e83c5c56822301132523
joanshpin <asiek898@gmail.com>
2017-04-10 23:14:49 +0200

    header intro fonts and first functionalities

--- js/app.js
+++ js/app.js
@@ -2,21 +2,32 @@ document.addEventListener('DOMContentLoaded', function(){
 
   var planData = document.querySelector(".planData");
 
-  var btnPlace = planData.querySelector(".place button");
-  var btnDates = planData.querySelector(".dates button");
-  var btnTransport = planData.querySelector(".transport button");
-  var btnAccomodation = planData.querySelector(".accomodation button");
+  var btns = planData.querySelectorAll("button");
 
-  var inpPlace = planData.querySelector(".place input");
-  var inpDates = planData.querySelector(".dates input");
-  var inpTransport = planData.querySelector(".transport input");
-  var inpAccomodation = planData.querySelector(".accomodation input");
+  // var btnPlace = planData.querySelector(".place button");
+  // var btnDates = planData.querySelector(".dates button");
+  // var btnTransport = planData.querySelector(".transport button");
+  // var btnAccomodation = planData.querySelector(".accomodation button");
+  //
+  // var inpPlace = planData.querySelector(".place input");
+  // var inpDates = planData.querySelector(".dates input");
+  // var inpTransport = planData.querySelector(".transport input");
+  // var inpAccomodation = planData.querySelector(".accomodation input");
 
-  btnPlace.addEventListener("click", function() {
-    // console.log("click");
-    if (inpPlace.length > 0) {
-      console.log("CLICK");
-    }
-  }); //closing addEventListener click
+  for (var i = 0; i < btns.length; i++) {
+    // var sibling = node.previousElementSibling;
+    btns[i].addEventListener("click", function() {
+      if (this.previousElementSibling.children[0].value.length > 0) {
+        spanVal = this.previousElementSibling.children[0].value;
+        this.previousElementSibling.children[1].innerText = spanVal;
+        this.previousElementSibling.children[0].style.display = "none";
+        this.innerText = "change";
+      }
+      else {
+        alert("please fill the blanks")
+      }
+    }); //closing addEventListener click
+  }
+
 
 }); // closing DOM
