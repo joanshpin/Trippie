370a5b1a36e5c264b978c6ae17a538049a02170d
joanshpin <asiek898@gmail.com>
2017-04-12 17:08:19 +0200

    google maps, firebase functionalities

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
 
   var planData = document.querySelector(".planData");
-
   var btns = planData.querySelectorAll(".planData button");
 
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
       var children = this.previousElementSibling.children;
-      console.log(children);
+      // console.log(children);
       if (children[0].value.length > 0) {
         var spanVal = children[0].value;
         children[1].innerText = spanVal;
         children[0].style.display = "none";
-        this.innerText = "change";
+        this.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
       }
       else {
         alert("please fill the blanks")
@@ -20,39 +19,13 @@ document.addEventListener('DOMContentLoaded', function(){
     }); //closing addEventListener click
   } //closing loop for btns
 
-  var btnAddMarker = document.querySelector(".addMarker");
-
-  btnAddMarker.addEventListener("click", function() {
-    var spanVal = document.querySelector(".planData .place h4 span").innerText;
-    console.log(spanVal);
-    var myApi = "AIzaSyAA6ikf7yth3jIMgE4dC22ZGcOmza-5Lww";
-    var googleUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+spanVal+"&key="+myApi;
-
-    console.log(googleUrl);
-    $.ajax({
-        url: googleUrl,
-        type: 'GET',
-        dataType: 'jsonp',
-        cache: false
-      }).done(function(response){
-        console.log(response);
-        // addPlaceToMap();
-      }).fail(function(error) {
-        console.log("error23", error);
-      });
-
-      function addPlaceToMap() {
-
-      }
-  })
-
+  // var btnAddMarker = document.querySelector(".addMarker");
+  // btnAddMarker.addEventListener("click", function() {
+  //   var spanVal = document.querySelector(".planData .place h4 span").innerText;
+  //   console.log(spanVal);
+  //   var myApi = "AIzaSyAA6ikf7yth3jIMgE4dC22ZGcOmza-5Lww";
+  //   var googleUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+spanVal+"&key="+myApi;
+  //   });
 
 
 }); // closing DOM
-//google maps api
-function initMap() {
-  var krakow = {lat: 50.061676, lng: 19.937770};
-  var map = new google.maps.Map(document.getElementById('map'), { zoom: 4, center: krakow });
-  var marker = new google.maps.Marker({ position: krakow, map: map });
-
-}
---
+++ js/firebase.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAMcorI0jK0zwGIrI5L5QcJNq_-cmhM6fc",
+    authDomain: "trippie-35bc7.firebaseapp.com",
+    databaseURL: "https://trippie-35bc7.firebaseio.com",
+    projectId: "trippie-35bc7",
+    storageBucket: "trippie-35bc7.appspot.com",
+    messagingSenderId: "73522637552"
+  };
+
+  //call to firebase
+  var app = firebase.initializeApp(config);
+  var cities = app.database().ref('cities');
+  cities.on("value", function(data) {
+      console.log(data.val());
+  }, function (error) {
+      console.log("Error: " + error.code);
+  });
+
+  var submitTripPlan = document.getElementById("submitTripPlan");
+  console.log(submitTripPlan, 'submitTripPlan');
+  submitTripPlan.addEventListener("click", function() {
+    console.log(submitTripPlan, 'submitTripPlan')
+    var titleVal = document.querySelector("#titleOfTrip h4 span").innerText;
+    var addressVal = document.querySelector("#addressDiv h4 span").innerText;
+    var dates1Val = document.querySelector("#dates1 h4 span").innerText;
+    var dates2Val = document.querySelector("#dates2 h4 span").innerText;
+    var transportVal = document.querySelector("#transport h4 span").innerText;
+    var acoomodationVal = document.querySelector("#accomodation h4 span").innerText;
+
+    var myPlaces = app.database().ref("places/myPlaces");
+    myPlaces.push({
+      "name of trip": titleVal,
+      "place": addressVal,
+      "arrival date": dates1Val,
+      "departure date": dates2Val,
+      "transport": transportVal,
+      "accomodation": acoomodationVal
+    });
+
+  })
+
+  // cities.push("Barcelona");
+}); //closing DOM
